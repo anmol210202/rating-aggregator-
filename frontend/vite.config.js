988b7fbef505b0784a749785954764aff4238e7f
Anmol SIngh <anmol210202@gmail.com>
2025-07-11 16:18:47 +0530

    Ending versel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,38 +1,22 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { loadEnv } from 'vite';
 import path from 'path'
-import { version } from './package.json';
 
-export default defineConfig(({ mode }) => {
-  const env = {
-    ...loadEnv(mode, process.cwd(), ""),
-  };
-  const processEnvValues = {
-    "process.env": Object.entries(env).reduce((prev, [key, val]) => ({
-      ...prev,
-      [key]: val,
-    }), {
-      VERSION: version,
-    }),
-  };
-
-  return {
-    plugins: [react()],
-    base: '/',
-    build: {
-      outDir: path.resolve(__dirname, 'dist'),
-      assetsDir: 'assets',
-      emptyOutDir: true,
-      rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, 'index.html')
-        },
-        output: {
-          assetFileNames: 'assets/[name]-[hash][extname]'
-        }
+export default defineConfig({
+  plugins: [react()],
+  base: '/configure/',
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
-    },
-    define: processEnvValues,
+    }
   }
-});
+})
